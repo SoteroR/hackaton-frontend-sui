1aea1cc053c4941e6ab53c3559ffde217fa1e95e
carla-vi <166115798+carla-vi@users.noreply.github.com>
2025-09-28 12:32:36 +0200

    Update Testnet package ID for crowdfunding

--- app/constants.ts
+++ app/constants.ts
@@ -1,6 +1,6 @@
 export const DEVNET_COUNTER_PACKAGE_ID =
   "0xTODO"; // replace if you deploy on Devnet
 export const TESTNET_COUNTER_PACKAGE_ID =
-  "0xa622dd35cf3283abcd5e3db6db4993a555868a658e11dc7a34b7f31133926685"; // ✅ your crowdfunding package ID
+  "0x3d7e0c9ff2fca8706bf8782a54e43ace49f710007460703051182ed84ebc54cd"; // crowdfunding package ID
 export const MAINNET_COUNTER_PACKAGE_ID =
   "0xTODO"; // leave placeholder if you don’t have mainnet deployment yet
